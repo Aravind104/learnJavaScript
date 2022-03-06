77fa8afa66a192c99fa18995c17ad8cc56d16849
Aravind104 <ontagodiaravind@gmail.com>
2022-03-06 11:22:55 +0530

    intial commit

---
+++ dist/index.dev.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// let a = '  '
+// if (typeof a === 'string' && a.trim().length > 0) {
+//     console.log('yes')
+// } else {
+//     console.log('no')
+// }
+// console.log(NaN === NaN)
+// console.log(NaN != NaN)
+// console.log(NaN == NaN)
+// Number Function 
+// k = Number('0.00024')
+// console.log(k)
+// console.log(typeof k)
+// parseInt function 
+// k = parseInt('012', 10) === 12;
+// console.log(k)
+// Math object is modeled on java's math class 
+// it contains
+// abs 
+// floor 
+// log 
+// max 
+// pow 
+// random 
+// round 
+// sin 
+// sqrt
+// Strings 
+// similar strings are equal java got that wrong 2008 
+// Strings are objects 
+// string.length   
+// console.log('aravind'.length)
+// k = String(24)
+// console.log(k)
+// console.log(typeof(k))
+// strings are objects in javascript they got methods 
+// charAt 
+// concat 
+// indexOf 
+// lastIndexOf 
+// match 
+// replace 
+// search 
+// slice 
+// split 
+// substring 
+// toLowerCase
+// toUpperCase
+// console.log('aravind'.toUpperCase())
+// console.log('aravind'.charAt(('aravind'.length) - 1))
+// Boolean
+// console.log(Boolean('yes'))
+// console.log(Boolean(false))
+// console.log(Boolean(null))
+// console.log(Boolean(undefined))
+// console.log(Boolean(NaN))
+// console.log(Boolean('')) +\8
+// console.log(Boolean("false"))
+// Falsy Values
+// false
+// null
+// undefined
+// NaN
+// 0
+// ""
+// variables
+var a = 10;
+console.log(a * 20);
+var burritoPrice = 10.99;
+var taxPrice = 0.05;
+var totalPrice = burritoPrice * taxPrice + burritoPrice;
